2298980bdd8bbce786de760d509d511a2a255991
DeLucca990 <pedruca2001@gmail.com>
2025-02-03 17:40:12 -0300

    conserta erros

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import sys
 
 if len(sys.argv) != 2:
     print("Deve ser passado um argumento para o programa")
-    sys.exit(1)
+    raise ValueError("Número de argumentos inválido")
 
 exp = sys.argv[1]
 
 result = 0
 current_number = ""
 current_operator = "+"
+
+if exp == '':
+    print("Expressão vazia")
+    raise ValueError("Expressão vazia")
+if exp[-1] in "+-":
+    print("Último caractere não pode ser um operador")
+    raise ValueError("Último caractere não pode ser um operador")
+if "+" not in exp or "-" not in exp:
+    print("Expressão inválida")
+    raise ValueError("Expressão inválida")
 
 for i in range(len(exp)):
     if exp[i].isdigit():
