c736e7c5650ec68158b467b787de43a73ad7acf3
DeLucca990 <pedruca2001@gmail.com>
2025-02-03 17:30:34 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Deve ser passado um argumento para o programa")
+    sys.exit(1)
+
+exp = sys.argv[1]
+
+result = 0
+current_number = ""
+current_operator = "+"
+
+for i in range(len(exp)):
+    if exp[i].isdigit():
+        current_number += exp[i]
+    elif exp[i] in "+-":
+        if current_operator == "+":
+            result += int(current_number)
+        elif current_operator == "-":
+            result -= int(current_number)
+        current_operator = exp[i]
+        current_number = ""
+
+if current_number:
+    if current_operator == "+":
+        result += int(current_number)
+    elif current_operator == "-":
+        result -= int(current_number)
+
+print(f"Saída: {result}")
