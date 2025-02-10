f93e51e76c1ff5d99b682bf89ac50095ee4f3ea7
DeLucca990 <pedruca2001@gmail.com>
2025-02-10 18:05:03 -0300

    feat: aula2

--- main.py
+++ main.py
@@ -1,40 +1,95 @@
-import sys
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
 
-if len(sys.argv) != 2:
-    print("Deve ser passado um argumento para o programa")
-    raise ValueError("Número de argumentos inválido")
+class Tokenizer:
+    def __init__(self, source):
+        self.source = source
+        self.position = 0
+        self.next = None
 
-exp = sys.argv[1]
+    def selectNext(self):
+        while self.position < len(self.source) and self.source[self.position].isspace():
+            self.position += 1
+        
+        if self.position >= len(self.source):
+            self.next = Token('EOF', None)
+            return
+        
+        current_char = self.source[self.position]
+        if current_char.isdigit():
+            start = self.position
+            while self.position < len(self.source) and self.source[self.position].isdigit():
+                self.position += 1
+            number_str = self.source[start:self.position]
+            self.next = Token('NUMBER', int(number_str))
+        elif current_char == '+':
+            self.next = Token('PLUS', None)
+            self.position += 1
+        elif current_char == '-':
+            self.next = Token('MINUS', None)
+            self.position += 1
+        else:
+            raise ValueError(f'Caractere inválido: {current_char}')
 
-result = 0
-current_number = ""
-current_operator = "+"
+class Parser:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+    
+    def parseExpression(self):
+        has_plus = False
+        has_minus = False
 
-if exp == '':
-    print("Expressão vazia")
-    raise ValueError("Expressão vazia")
-if exp[-1] in "+-":
-    print("Último caractere não pode ser um operador")
-    raise ValueError("Último caractere não pode ser um operador")
-if "+" not in exp or "-" not in exp:
-    print("Expressão inválida")
-    raise ValueError("Expressão inválida")
+        token = self.tokenizer.next
+        if token.type != "NUMBER":
+            raise ValueError(f'Número esperado, mas obtido: {token}')
+        result = token.value
 
-for i in range(len(exp)):
-    if exp[i].isdigit():
-        current_number += exp[i]
-    elif exp[i] in "+-":
-        if current_operator == "+":
-            result += int(current_number)
-        elif current_operator == "-":
-            result -= int(current_number)
-        current_operator = exp[i]
-        current_number = ""
+        self.tokenizer.selectNext()
+        while self.tokenizer.next.type in ("PLUS", "MINUS"):
+            op = self.tokenizer.next
+            if op.type == 'PLUS':
+                has_plus = True
+            if op.type == 'MINUS':
+                has_minus = True
+            
+            self.tokenizer.selectNext()
+            token = self.tokenizer.next
+            if token.type != "NUMBER":
+                raise ValueError(f'Número esperado, mas obtido: {token}')
+            if op.type == 'PLUS':
+                result += token.value
+            if op.type == 'MINUS':
+                result -= token.value
+            self.tokenizer.selectNext()
+        
+        if not(has_plus or has_minus):
+            raise ValueError('Expressão inválida, esperado + ou -')
+        
+        return result
 
-if current_number:
-    if current_operator == "+":
-        result += int(current_number)
-    elif current_operator == "-":
-        result -= int(current_number)
+    @staticmethod
+    def run(code):
+        tokenizer = Tokenizer(code)
+        tokenizer.selectNext()
+        parser = Parser(tokenizer)
+        result = parser.parseExpression()
 
-print(result)
+        if tokenizer.next.type != "EOF":
+            raise ValueError(f'EOF esperado, mas obtido: {tokenizer.next}')
+        return result
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print('python3 main.py <expressão>')
+        raise ValueError('Argumentos inválidos')
+
+    code = sys.argv[1]
+
+    if code == "":
+        raise ValueError('Expressão inválida')
+
+    print(Parser.run(code))
