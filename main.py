d702d4e83fde9fff5a376050cfd4f5cbaa3116c8
DeLucca990 <pedruca2001@gmail.com>
2025-03-18 10:02:57 -0300

    feat: roteiro 5

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class PrePro:
     @staticmethod
     def filter(code):
         code = re.sub(r'\/\/.*', '', code)
+        code = re.sub(r'\/\*[\s\S]*?\*\/', '', code)
         return code
 
 class Token:
@@ -14,12 +15,25 @@ class Token:
     
     def __str__(self):
         return f"Token({self.type}, {self.value})"
+    
+class SymbolTable:
+    def __init__(self):
+        self.symbols = {}
+    def get(self, key):
+        if key not in self.symbols:
+            raise ValueError(f'Variável não declarada: {key}')
+        return self.symbols[key]
+    def set(self, key, value):
+        self.symbols[key] = value
 
 class Tokenizer:
     def __init__(self, source):
         self.source = source
         self.position = 0
         self.next = None
+        self.keywords = {
+            "Println": "PRINTLN",
+        }
 
     def selectNext(self):
         while self.position < len(self.source) and self.source[self.position].isspace():
@@ -30,12 +44,24 @@ class Tokenizer:
             return
         
         current_char = self.source[self.position]
+        
+        # Se for dígito
         if current_char.isdigit():
             start = self.position
             while self.position < len(self.source) and self.source[self.position].isdigit():
                 self.position += 1
             number_str = self.source[start:self.position]
             self.next = Token('NUMBER', int(number_str))
+        # Se for letra
+        elif current_char.isalpha():
+            start = self.position
+            while self.position < len(self.source) and (self.source[self.position].isalnum() or self.source[self.position] == '_'):
+                self.position += 1
+            identifier = self.source[start:self.position]
+            if identifier in self.keywords:
+                self.next = Token(self.keywords[identifier], None)
+            else:
+                self.next = Token('IDENTIFIER', identifier)
         elif current_char == '+':
             self.next = Token('PLUS', None)
             self.position += 1
@@ -54,6 +80,15 @@ class Tokenizer:
         elif current_char == ')':
             self.next = Token('RPAREN', None)
             self.position += 1
+        elif current_char == '{':
+            self.next = Token('LBRACE', None)
+            self.position += 1
+        elif current_char == '}':
+            self.next = Token('RBRACE', None)
+            self.position += 1
+        elif current_char == '=':
+            self.next = Token('ASSIGN', None)
+            self.position += 1
         else:
             raise ValueError(f'Caractere inválido: {current_char}')
 
@@ -71,18 +106,18 @@ class BinOp(Node):
         self.value = value
         self.children = [left, right]
 
-    def evaluate(self):
+    def evaluate(self, symbol_table):
         if self.value == '+':
-            return self.children[0].evaluate() + self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) + self.children[1].evaluate(symbol_table)
         elif self.value == '-':
-            return self.children[0].evaluate() - self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) - self.children[1].evaluate(symbol_table)
         elif self.value == '*':
-            return self.children[0].evaluate() * self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) * self.children[1].evaluate(symbol_table)
         elif self.value == '/':
-            divisor = self.children[1].evaluate()
+            divisor = self.children[1].evaluate(symbol_table)
             if divisor == 0:
                 raise ValueError('Divisão por zero')
-            return self.children[0].evaluate() // divisor
+            return self.children[0].evaluate(symbol_table) // divisor
 
 class UnOp(Node):
     def __init__(self, value, child):
@@ -90,31 +125,106 @@ class UnOp(Node):
         self.value = value
         self.children = [child]
 
-    def evaluate(self):
+    def evaluate(self, symbol_table):
         if self.value == '+':
-            return self.children[0].evaluate()
+            return self.children[0].evaluate(symbol_table)
         elif self.value == '-':
-            return -self.children[0].evaluate()
+            return -self.children[0].evaluate(symbol_table)
 
 class IntVal(Node):
     def __init__(self, value):
         super().__init__()
         self.value = value
 
-    def evaluate(self):
+    def evaluate(self, symbol_table):
         return self.value
 
 class NoOp(Node):
     def __init__(self):
         super().__init__()
 
-    def evaluate(self):
+    def evaluate(self, symbol_table):
         return 0
+
+class Block(Node):
+    def __init__(self, statements):
+        self.children = statements
+    
+    def evaluate(self, symbol_table):
+        for statement in self.children:
+            statement.evaluate(symbol_table)
+
+class Println(Node):
+    def __init__(self, expression):
+        self.children = [expression]
+    
+    def evaluate(self, symbol_table):
+        result = self.children[0].evaluate(symbol_table)
+        print(result)
+
+class Identifier(Node):
+    def __init__(self, name):
+        super().__init__()
+        self.value = name
+    
+    def evaluate(self, symbol_table):
+        return symbol_table.get(self.value)
+
+class Assignment(Node):
+    def __init__(self, identifier, expression):
+        super().__init__()
+        self.children = [identifier, expression]
+    
+    def evaluate(self, symbol_table):
+        value = self.children[1].evaluate(symbol_table)
+        symbol_table.set(self.children[0].value, value)
+        return value
 
 class Parser:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
         self.tokenizer.selectNext()
+
+    def parseStatement(self):
+        # Instrução println: Println(expr)
+        if self.tokenizer.next.type == 'PRINTLN':
+            self.tokenizer.selectNext()
+            if self.tokenizer.next.type != 'LPAREN':
+                raise ValueError('Parêntese de abertura esperado')
+            self.tokenizer.selectNext()
+            expression = self.parseExpression()
+            if self.tokenizer.next.type != 'RPAREN':
+                raise ValueError('Parêntese de fechamento esperado')
+            self.tokenizer.selectNext()
+            return Println(expression)
+
+        # Bloco de instruções {...}
+        elif self.tokenizer.next.type == 'LBRACE':
+            self.tokenizer.selectNext()
+            statements = []
+            while self.tokenizer.next.type != 'RBRACE':
+                statement = self.parseStatement()
+                statements.append(statement)
+            self.tokenizer.selectNext()
+            return Block(statements)
+
+        # Atribuição: identificador = expr
+        elif self.tokenizer.next.type == 'IDENTIFIER':
+            identifier = Identifier(self.tokenizer.next.value)
+            self.tokenizer.selectNext()
+
+            if self.tokenizer.next.type != 'ASSIGN':
+                raise ValueError('Esperado = após identificador')
+            self.tokenizer.selectNext()
+
+            expression = self.parseExpression()
+            
+            return Assignment(identifier, expression)
+
+        # Expressão como instrução
+        else:
+            expression = self.parseExpression()
+            return expression
     
     def parseExpression(self):
         result = self.parseTerm()
@@ -143,6 +253,10 @@ class Parser:
             result = IntVal(self.tokenizer.next.value)
             self.tokenizer.selectNext()
             return result
+        elif self.tokenizer.next.type == 'IDENTIFIER':
+            result = Identifier(self.tokenizer.next.value)
+            self.tokenizer.selectNext()
+            return result
         elif self.tokenizer.next.type == 'PLUS':
             self.tokenizer.selectNext()
             return UnOp('+', self.parseFactor())
@@ -158,12 +272,13 @@ class Parser:
             return result
         else:
             raise ValueError("Fator inválido")
-
+    
     def parse(self):
-        result = self.parseExpression()
-        if self.tokenizer.next.type != 'EOF':
-            raise ValueError(f'EOF esperado, mas obtido: {self.tokenizer.next}')
-        return result
+        statments = []
+        while self.tokenizer.next.type != 'EOF':
+            statment = self.parseStatement()
+            statments.append(statment)
+        return Block(statments)
 
 def main(file):
     try:
@@ -177,12 +292,12 @@ def main(file):
         
         tokenizer = Tokenizer(filtered_code)
         parser = Parser(tokenizer)
-
         ast = parser.parse()
 
-        result = ast.evaluate()
+        symbol_table = SymbolTable()
 
-        print(result)
+        result = ast.evaluate(symbol_table)
+
         return result
     except FileNotFoundError:
         print(f"Erro: Arquivo '{file}' não encontrado.")
--- teste.go
+++ teste.go
@@ -1,1 +1,6 @@
-11+22-33 // Teste teste
+{ // \n obrigatório
+	x1 = 3
+	y2 = 4
+	z_final = x1 + y2
+	Println(z_final)
+}
